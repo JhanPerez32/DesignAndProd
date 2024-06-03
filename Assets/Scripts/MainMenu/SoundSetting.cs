39e811e9e19ce75230fd5f937015488b4867d502
JhanPerez32 <jhankhendrickperez@gmail.com>
2024-06-03 16:17:23 +0800

    Update
    Added:
    Audio Settings
    -Temporary BGM for testing the setting
    -Percentage value of the slider
    -Load and Saves the set Volume
    
    Loading Screen:
    -Loading Progress
    -Percentage value
    -TestLoadingScene (for testing the Loading Screen)

--- Assets/Scripts/MainMenu/SoundSetting.cs
+++ Assets/Scripts/MainMenu/SoundSetting.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundSetting : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
+    [SerializeField] TextMeshProUGUI sliderText;
 
     void Start()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
         {
             PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
         }
-        else
+
+        Load();
+
+        sliderText.text = (volumeSlider.value * 100).ToString("0") + "%";
+
+        volumeSlider.onValueChanged.AddListener((v) =>
         {
-            Load();
-        }
+            sliderText.text = (v * 100).ToString("0") + "%";
+        });
     }
 
     public void ChangeVolume()
