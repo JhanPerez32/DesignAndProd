947e27fa6b3821dee54098578f58877a0c3bd7ca
JhanPerez32 <jhankhendrickperez@gmail.com>
2024-06-09 16:44:26 +0800

    Player Death Mechanic Game Over UI will show upon Death

--- Assets/Scripts/GameOver/PlayerDeath.cs
+++ Assets/Scripts/GameOver/PlayerDeath.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CharacterController characterController;
+    public GameObject gameOverScreen;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+
+        if (gameOverScreen != null)
+        {
+            gameOverScreen.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        
+        if (hit.gameObject.CompareTag("Obstacle"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Time.timeScale = 0;
+        ShowGameOverScreen();
+        Debug.Log("Player Died");
+    }
+
+    private void ShowGameOverScreen()
+    {
+        if (gameOverScreen != null)
+        {
+            gameOverScreen.SetActive(true);
+        }
     }
 }
