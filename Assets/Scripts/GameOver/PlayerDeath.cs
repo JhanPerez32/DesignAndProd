5f1da0bd14ff7e00faea1d8ed56712ce85e5d291
JhanPerez32 <jhankhendrickperez@gmail.com>
2024-06-17 19:24:31 +0800

    Game Over Script Improved

--- Assets/Scripts/GameOver/PlayerDeath.cs
+++ Assets/Scripts/GameOver/PlayerDeath.cs
@@ -6,17 +6,11 @@ using UnityEngine.SceneManagement;
 public class PlayerDeath : MonoBehaviour
 {
     public PlayerMove playerMoveScript;
-    public CharacterController characterController;
     public GameObject gameOverScreen;
-
-    [SerializeField] LoadingManager loadingManager;
-
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
         playerMoveScript = GetComponent<PlayerMove>();
-        loadingManager = FindObjectOfType<LoadingManager>();
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
@@ -33,21 +27,6 @@ public class PlayerDeath : MonoBehaviour
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         gameOverScreen.SetActive(true);
-        playerMoveScript.enabled = false; // Add this line to disable PlayerMove script
-    }
-
-    public void RestartGame()
-    {
-        Time.timeScale = 1f;
-        characterController.enabled = false;
-
-        if (loadingManager != null)
-        {
-            loadingManager.LoadScene(SceneManager.GetActiveScene().name); // Uses the LoadingManager to load the Current scene
-        }
-        else
-        {
-            Debug.LogError("LoadingManager not found in the scene.");
-        }
+        playerMoveScript.enabled = false; //Disable the PlayerMove script
     }
 }
