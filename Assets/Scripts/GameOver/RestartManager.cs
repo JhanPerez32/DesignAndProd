badad289c2c9b5814d33767eeae3dd6c92dcf023
JhanPerez32 <jhankhendrickperez@gmail.com>
2024-06-17 19:24:31 +0800

    Game Over Script Improved

--- Assets/Scripts/GameOver/RestartManager.cs
+++ Assets/Scripts/GameOver/RestartManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] LoadingManager loadingManager;
+    public CharacterController characterController;
+    [SerializeField] GameObject playerGameObject;
+
+    private void Start()
     {
-        
+        if (playerGameObject != null)
+        {
+            characterController = playerGameObject.GetComponent<CharacterController>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RestartGame()
     {
-        
+        Time.timeScale = 1f;
+        characterController.enabled = false;
+
+        if (loadingManager != null)
+        {
+            loadingManager.LoadScene(SceneManager.GetActiveScene().name); // Uses the LoadingManager to load the Current scene
+        }
+        else
+        {
+            Debug.LogError("LoadingManager not found in the scene.");
+        }
     }
 }
