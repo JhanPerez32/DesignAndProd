5e76976e3c4c4090b81859009334a988c440a41c
CIIT IT <it@ciit.edu.ph>
2024-06-27 15:22:30 +0800

    Player Death from Enemy

--- Assets/Scripts/GameOver/PlayerDeath.cs
+++ Assets/Scripts/GameOver/PlayerDeath.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pathways;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    public PlayerMove playerMoveScript;
-    public GameObject gameOverScreen;
+    [SerializeField] EnemyController enemyController;
+    [SerializeField] float distanceThreshold;
+    [SerializeField] PlayerMove playerMoveScript;
+    //[SerializeField] GameObject gameOverScreen;
 
+    public UnityEvent Hit;
     void Start()
     {
         playerMoveScript = GetComponent<PlayerMove>();
+        if (!enemyController)
+        {
+            enemyController = GetComponent<EnemyController>();
+        }
+    }
+
+    private void Update()
+    {
+        if(enemyController.distanceToPlayer < distanceThreshold)
+        {
+            Die();
+            Debug.Log("Enemy Hit");
+
+        }
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
@@ -21,12 +40,13 @@ public class PlayerDeath : MonoBehaviour
         }
     }
 
-    private void Die()
+    public void Die()
     {
         Time.timeScale = 0f;
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
-        gameOverScreen.SetActive(true);
+        //gameOverScreen.SetActive(true);
         playerMoveScript.enabled = false; //Disable the PlayerMove script
+        Hit.Invoke();
     }
 }
