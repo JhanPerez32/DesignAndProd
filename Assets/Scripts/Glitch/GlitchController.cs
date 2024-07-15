e6a37c23434e30d1602c1000c400903a5f36e45c
JhanPerez32 <jhankhendrickperez@gmail.com>
2024-07-15 15:32:55 +0800

    Glitch
    Volume Profile of Glitch can be control via script
    Inicrease Glitch when Distance to Player gets to reduce

--- Assets/Scripts/Glitch/GlitchController.cs
+++ Assets/Scripts/Glitch/GlitchController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using URPGlitch.Runtime.AnalogGlitch;
 using URPGlitch.Runtime.DigitalGlitch;
 
 public class GlitchController : MonoBehaviour
 {
     public Volume volume;
 
-    [Range(0f, 1f)]
+    [Header ("Digital Glitch")]
+    [Range (0f, 1f)]
     public float glitchIntensity;
 
+    [Header("Analog Glicth")]
+    [Range(0f, 1f)]
+    public float scanLineJitterIntensity;
+    [Range(0f, 1f)]
+    public float verticalJumpIntensity;
+    [Range(0f, 1f)]
+    public float horizontalShakeIntensity;
+    [Range(0f, 1f)]
+    public float colorDriftIntensity;
+
     private DigitalGlitchVolume digitalGlitchVolume;
+    private AnalogGlitchVolume analogGlitchVolume;
 
     private void Start()
     {
@@ -18,10 +31,30 @@ public class GlitchController : MonoBehaviour
         {
             VolumeProfile profile = volume.profile;
 
+            //Setting the Intensities
+
+            //Digital Glitch
             if (profile != null && profile.TryGet(out digitalGlitchVolume))
             {
-                // Initially set the intensity
                 digitalGlitchVolume.intensity.value = glitchIntensity;
+            }
+
+            //Analog Glitch
+            if(profile != null && profile.TryGet(out analogGlitchVolume))
+            {
+                analogGlitchVolume.scanLineJitter.value = scanLineJitterIntensity;
+            }
+            if (profile != null && profile.TryGet(out analogGlitchVolume))
+            {
+                analogGlitchVolume.verticalJump.value = verticalJumpIntensity;
+            }
+            if (profile != null && profile.TryGet(out analogGlitchVolume))
+            {
+                analogGlitchVolume.horizontalShake.value = horizontalShakeIntensity;
+            }
+            if (profile != null && profile.TryGet(out analogGlitchVolume))
+            {
+                analogGlitchVolume.colorDrift.value = colorDriftIntensity;
             }
         }
     }
@@ -30,7 +63,25 @@ public class GlitchController : MonoBehaviour
     {
         if (digitalGlitchVolume != null)
         {
-            digitalGlitchVolume.intensity.value = glitchIntensity;
+            IncreaseDigitalIntensity();
         }
+
+        if(analogGlitchVolume != null)
+        {
+            IncreaseAnalogIntensity();
+        }
+    }
+
+    void IncreaseDigitalIntensity()
+    {
+        digitalGlitchVolume.intensity.value = glitchIntensity;
+    }
+
+    void IncreaseAnalogIntensity()
+    {
+        analogGlitchVolume.scanLineJitter.value = scanLineJitterIntensity;
+        analogGlitchVolume.verticalJump.value = verticalJumpIntensity;
+        analogGlitchVolume.horizontalShake.value = horizontalShakeIntensity;
+        analogGlitchVolume.colorDrift.value = colorDriftIntensity;
     }
 }
